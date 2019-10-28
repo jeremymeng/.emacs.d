03430b138511887639764dd13ab6890c9f648f28
Jeremy Meng <jeremy.ymeng@gmail.com>
2019-10-27 22:33:19 -0700

    Add more packages

--- init.el
+++ init.el
@@ -1,20 +1,35 @@
-;; -*- mode: emacs-lisp -*-
+;;; init.el --- emacs configuration -*- mode: emacs-lisp; lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+;; Set default font
+(set-face-attribute 'default nil
+                    :family "SF Mono"
+                    :height 100
+                    :weight 'normal
+                    :width 'normal)
+
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(setq user-emacs-directory "~/.tryout/")
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(when (not package-archive-contents)
+(unless package-archive-contents
   (package-refresh-contents))
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
   (package-install 'use-package))
-
+(setq use-package-verbose t)
 (eval-when-compile
   (require 'use-package))
+
+(setq gc-cons-threshold 50000000)
 
 ;; allow M-SPC as a prefix
 (global-set-key (kbd "M-SPC") nil)
@@ -34,7 +49,8 @@
   :ensure t
   :config
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
-  :bind ("M-SPC g s" . magit-status)
+  :bind
+  ("M-SPC g s" . magit-status)
   :commands magit-status)
 
 (use-package forge
@@ -55,6 +71,8 @@
 
 (use-package markdown-mode
   :defer t
+  :config
+  (setq markdown-fontify-code-blocks-natively t)
   :ensure t)
 
 (use-package company
@@ -91,10 +109,10 @@
   :config
   (setq ibuffer-formats
         '((mark modified read-only " " (name 16 16) " "
-                (size 9 -1 :right) " " (mode 16 16)
+                (size 9 -1 :right) " " (mode 16 16 :left :elide)
                 " " (process 8 -1) " " filename)
           (mark " " (name 16 -1) " " filename))
-        ibuffer-elide-long-columns t
+        ;; ibuffer-elide-long-columns t
         ibuffer-eliding-string "&"))
 
 (use-package counsel
@@ -113,9 +131,15 @@
   :bind
   ("M-SPC s s" . swiper)
   ("M-SPC s r" . counsel-rg)
+  ("M-SPC f f" . counsel-file-jump)
   ("M-x" . counsel-M-x)
   ("C-x b" . ivy-switch-buffer)
   ("M-SPC b b" . ivy-switch-buffer)
+  ("<f1> f" . 'counsel-describe-function)
+  ("<f1> v" . 'counsel-describe-variable)
+  ("<f1> l" . 'counsel-find-library)
+  ("<f2> i" . 'counsel-info-lookup-symbol)
+  ("<f2> u" . 'counsel-unicode-char)
   :diminish (counsel-mode . ""))
 (counsel-mode t)
 
@@ -176,13 +200,87 @@
 (use-package projectile
   :diminish projectile-mode
   :init
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (setq projectile-completion-system 'ivy)
+  :bind-keymap
+  ("M-SPC p" . projectile-command-map)
   :config
-  (projectile-mode +1)
+  (projectile-mode 1)
   :ensure t)
 
 (use-package smex
   :ensure t)
+
+(use-package expand-region
+  :bind
+  ("M-SPC v" . er/expand-region)
+  :ensure t)
+
+(use-package which-key
+  :config
+  (which-key-mode 1)
+  :ensure t)
+
+(use-package smartparens
+  :ensure t
+  :config
+  (setq sp-show-pair-from-inside nil)
+  (require 'smartparens-config)
+  (add-hook 'prog-mode-hook 'smartparens-mode)
+  :diminish smartparens-mode)
+
+(use-package savehist
+  :config
+  (setq savehist-additional-variables
+        ;; search entries
+        '(search-ring regexp-search-ring)
+        ;; save every minute
+        savehist-autosave-interval 120
+        ;; keep the home clean
+        savehist-file (expand-file-name "savehist" user-emacs-directory))
+  (savehist-mode 1))
+
+(use-package recentf
+  :config
+  (setq recentf-save-file (expand-file-name "recentf" user-emacs-directory)
+        recentf-max-saved-items 500
+        recentf-max-menu-items 15
+        ;; disable recentf-cleanup on Emacs start, because it can cause
+        ;; problems with remote files
+        recentf-auto-cleanup 'never)
+  (recentf-mode 1))
+
+(use-package move-text
+  :ensure t
+  :bind
+  (([(meta up)] . move-text-up)
+   ([(meta down)] . move-text-down)))
+
+(use-package rainbow-delimiters
+  :ensure t)
+
+(use-package rainbow-mode
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-mode))
+
+(use-package multiple-cursors
+  :ensure t
+  :bind (
+         ("M-SPC s m a" . mc/mark-all-dwim)
+         ("M-SPC s m b" . mc/mark-all-like-this)
+         ("M-SPC s m m" . mc/mark-more-like-this-extended)
+         ("M-SPC s m r" . mc/edit-lines)
+         ("M-SPC s m s l" . mc/insert-letters)
+         ("M-SPC s m s m" . mc/mark-sgml-tag-pair)
+         ("M-SPC s m s n" . mc/insert-numbers)
+         ("M-SPC s m s r" . set-rectangular-region-anchor)
+         ("M-SPC s m s s" . mc/sort-regions)
+         ("M-SPC s m s t" . mc/reverse-regions)
+         ;; ("M-." . mc/mark-next-like-this)
+         ;; ("M-," . mc/unmark-next-like-this)
+         ("C-S-<mouse-1>" . mc/add-cursor-on-click)))
+
+(server-start)
 
 (load-theme 'leuven 'no-confirm)
 
@@ -192,7 +290,7 @@
 (line-number-mode t)
 (column-number-mode t)
 (setq-default transient-mark-mode t)
-(setq fill-column 78)
+(setq fill-column 80)
 (setq-default indent-tabs-mode nil)
 (setq frame-title-format
       '("%S: " (buffer-file-name "%f"
@@ -203,10 +301,18 @@
        ;; Maximum colors
        (setq font-lock-maximum-decoration t)))
 
+(global-auto-revert-mode t)
+
+(defun my/text-mode-setup ()
+  "Configure text mode hooks."
+  (olivetti-mode 1)
+  (olivetti-set-width 140)
+  (flyspell-mode 1)
+  (visual-line-mode 1))
+
 (dolist (hook '(text-mode-hook))
-  (add-hook hook (lambda () (flyspell-mode 1))))
-(dolist (hook '(text-mode-hook))
-  (add-hook hook (lambda () (auto-fill-mode 1))))
+  (add-hook hook (lambda () (my/text-mode-setup))))
+
 (dolist (hook '(change-log-mode-hook log-edit-mode-hook))
   (add-hook hook (lambda () (flyspell-mode -1))))
 
@@ -226,6 +332,8 @@
         try-complete-lisp-symbol))
 
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+(tool-bar-mode -1)
 
 ;;
 ;; Set key macros
@@ -288,3 +396,5 @@
  ;; If there is more than one, they won't work right.
  '(font-lock-comment-delimiter-face ((((class color) (min-colors 16) (background dark)) (:foreground "SteelBlue"))))
  '(font-lock-comment-face ((((class color) (min-colors 16) (background dark)) (:foreground "SteelBlue")))))
+
+;;; init.el ends here
